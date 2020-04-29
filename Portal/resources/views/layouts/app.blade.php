578932f799bab95468000b378e4228760a58161e
slowestwind <slowestwind@gmail.com>
2020-04-29 22:34:38 +0530

    removal of un-necessary files

--- Portal/resources/views/layouts/app.blade.php
+++ Portal/resources/views/layouts/app.blade.php
@@ -31,12 +31,6 @@
     <link href="../build/css/custom.min.css" rel="stylesheet">
 
 
-
-
-
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
