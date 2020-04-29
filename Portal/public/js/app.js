9e851fe76ffeb4d48cc4c508f02ab9100849bb51
surajsinghbisht054 <surajsinghbisht054@gmail.com>
2020-04-30 00:23:48 +0530

    Revert "dashboard template updated"
    This reverts commit 25789d80e0c16cd46b2f8f952db474d3d58865aa.

--- Portal/public/js/app.js
+++ Portal/public/js/app.js
@@ -49935,8 +49935,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /home/slowestwind/Desktop/onProcessingProjects/UniversityPortal/Portal/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /home/slowestwind/Desktop/onProcessingProjects/UniversityPortal/Portal/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /home/himanshucool/Desktop/Blaregroup/universityportal/Portal/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /home/himanshucool/Desktop/Blaregroup/universityportal/Portal/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
