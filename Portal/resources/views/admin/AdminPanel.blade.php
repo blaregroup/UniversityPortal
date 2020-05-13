d37f9d2d789d0b0b38e65ce9062985f7c1a9f61a
himanshusharmahub <himanshusharma2972@gmail.com>
2020-05-13 19:36:19 +0530

    hover menu

--- Portal/resources/views/admin/AdminPanel.blade.php
+++ Portal/resources/views/admin/AdminPanel.blade.php
@@ -5,17 +5,17 @@
 @section('lpanel')
     <div class=" border-right position-fixed " id="sidebar-wrapper" style="left:0;top:105px; background: #30336b" >
         <div class="list-group list-group-flush" style="width:13rem;">
-            <a href="/home" class="list-group-item list-group-item-action text-white" style="background: #30336b;"><span class="fa fa-home m-2 "></span> Home</a>
+            <a href="/home" class="list-group-item list-group-item-action text-white" ><span class="fa fa-home m-2 "></span> Home</a>
             
-            <a href="/profile/personal" class="list-group-item list-group-item-action text-white" style="background: #30336b;"><span class="fa fa-user-circle m-2"></span> Profile</a>
-            <a href="/admin/add" class="list-group-item list-group-item-action text-white" style="background: #30336b;"><span class="m-2 fa fa-users"></span>  Users</a>        
-            <a href="/admin/perm" class="list-group-item list-group-item-action text-white" style="background: #30336b;"> 
+            <a href="/profile/personal" class="list-group-item list-group-item-action text-white"><span class="fa fa-user-circle m-2"></span> Profile</a>
+            <a href="/admin/add" class="list-group-item list-group-item-action text-white"><span class="m-2 fa fa-users"></span>  Users</a>        
+            <a href="/admin/perm" class="list-group-item list-group-item-action text-white"> 
                 <span class=" fa fa-user-secret m-2" style="background: #30336b;"></span>
             Privilage</a>
-            <a href="/upload" class="list-group-item list-group-item-action text-white" style="background: #30336b;"><span class="m-2 fa fa-cloud-upload"></span> Uploads</a>
-            <a href="/admin/course" class="list-group-item list-group-item-action text-white" style="background: #30336b;">
+            <a href="/upload" class="list-group-item list-group-item-action text-white"><span class="m-2 fa fa-cloud-upload"></span> Uploads</a>
+            <a href="/admin/course" class="list-group-item list-group-item-action text-white" >
                 <span class=" fa fa-book m-2"></span>Course</a>
-            <a href="/notice" class="list-group-item list-group-item-action text-white" style="background: #30336b;"> 
+            <a href="/notice" class="list-group-item list-group-item-action text-white"> 
                 <span class="fa fa-envelope m-2"></span> Notification</a>
         </div>
     </div>
